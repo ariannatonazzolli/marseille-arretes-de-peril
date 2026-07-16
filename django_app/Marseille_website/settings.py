b4d8b9e590272120888fe271360a349a92da5c3f
Arianna Tonazzolli <arianna.tonazzolli@gmail.com>
2026-07-16 10:17:12 +0200

    Fix: import os

--- django_app/Marseille_website/settings.py
+++ django_app/Marseille_website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 import dj_database_url
 
@@ -21,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
